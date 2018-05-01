4b0f22b57adadb3326adaad1fe9f91c8ae0d62df
Luke Mwila <outlier.developer@gmail.com>
2018-05-01 09:04:24 +0200

    using VanillaJS OOP to create a book from the form data

---
+++ app.js
@@ -0,0 +1,25 @@
+// Book Constructor
+function Book(title, author, isbn){
+    this.title = title
+    this.author = author
+    this.isbn = isbn
+}
+
+// UI Constructor
+function UI(){
+
+}
+
+//Event Listeners
+document.getElementById('book-form').addEventListener('submit', 
+function(e){
+    // get form values
+    const title = document.getElementById('title').value,
+          author = document.getElementById('author').value,
+          isbn = document.getElementById('isbn').value
+
+    // Instantiate a book
+    const book = new Book(title, author, isbn)
+
+    e.preventDefault()
+})
